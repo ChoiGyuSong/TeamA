591fbccff29914cf26d339fd586ecb3371a1e822
123 <133838215+Elce08@users.noreply.github.com>
2023-06-14 14:16:35 +0900

    123

--- Assets/Scripts/Player/Player3.cs
+++ Assets/Scripts/Player/Player3.cs
@@ -4,15 +4,4 @@ using UnityEngine;
 
 public class Player3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
